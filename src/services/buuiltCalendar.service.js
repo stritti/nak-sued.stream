dc5517b0db76382af48f90b98fa5ce07123f18f7
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2021-02-12 15:48:11 +0100

    remove logs

--- src/services/buuiltCalendar.service.js
+++ src/services/buuiltCalendar.service.js
@@ -2,11 +2,7 @@ import axios from 'axios'
 
 const buuiltCalendarService = {
   async getList (feed) {
-    // const response = await axios.get('https://www.nak-tuttlingen.de/api/tree/32/C30834B8-6C89-11EB-B7A1-47F70101BD0E/export')
-    // const response = await axios.get('https://www.nak-tuttlingen.de/api/tree/33/C057DC10-6D1E-11EB-A445-47010201BD0E/export')
-    console.log('Feed', feed)
     const response = await axios.get(feed)
-    console.log(response)
     let resultList = response.data.items.map((item) => {
       return {
         name: item[65],
