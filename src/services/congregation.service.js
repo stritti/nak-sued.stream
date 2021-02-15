2fe88cf681f69c0641e6e7c64df1d486a9dee616
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2021-02-15 12:15:04 +0100

    remove logging

--- src/services/congregation.service.js
+++ src/services/congregation.service.js
@@ -15,8 +15,6 @@ const congregationService = {
     return resultList
   },
   async get (slug, pin) {
-    console.log('slug', slug)
-    console.log('pin', pin)
     const filter = encodeURIComponent(`AND({slug} = "${slug}", {pin} = "${pin}")`)
     const response = await airtableBase.get(`${BASE}?view=${ACTIVE_VIEW}&filterByFormula=${filter}&maxRecords=1`)
     const resultList = response.data.records.map((item) => {
