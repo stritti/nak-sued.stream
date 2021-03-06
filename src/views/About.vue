b82e8983715e1ab6541af35b28d6ccc8088d58c3
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2021-03-06 20:13:54 +0100

    updated about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="about">
     <h1>Über NAK-Sued.stream</h1>
-    <p>Kommt bald ...</p>
+    <p>
+      Diese Webseite wurde ehrenamtlich privat erstellt und wird auch privat betrieben.
+    </p>
+    <p>
+      Falls Sie den Betrieb dieser Webseite unterstützen möchten:
+      <a
+        href="https://ko-fi.com/J3J33A8DT"
+        target="_blank"
+      ><img
+        height="36"
+        style="border:0px;height:36px;"
+        src="https://cdn.ko-fi.com/cdn/kofi2.png?v=2"
+        border="0"
+        alt="Buy Me a Coffee at ko-fi.com"
+      ></a>
+    </p>
+    <p>
+      Für die Angaben in den Übersichten der Live Stream, übernehmen die angegebenen
+      Ansprechpartner:innen die Verantwortung.
+    </p>
   </div>
 </template>
 
