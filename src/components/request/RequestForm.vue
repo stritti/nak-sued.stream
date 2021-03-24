b7c1fe4a753ea917d435ae827c027d745f252d52
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2021-03-24 18:06:55 +0100

    refactored request form

--- src/components/request/RequestForm.vue
+++ src/components/request/RequestForm.vue
@@ -1,145 +1,198 @@
 <template>
-  <b-form @submit.prevent="submitRequest">
-    <b-form-group
-      label="Feed-URL"
-    >
-      <b-form-input
-        v-model="requestData.Feed"
-        type="url"
-        required
-        disabled
-      />
-    </b-form-group>
-    <b-form-group
-      label="Seiten-Titel"
-      description="z.B. 'Gemeinde Stockach'"
-    >
-      <b-form-input
-        v-model="requestData.Name"
-        placeholder="Gemeinde XYZ"
-        type="text"
-        required
-      />
-    </b-form-group>
-    <b-form-group
-      label="gewünschter Link"
-      description="Gewünschter Link für den Stream. Muss eindeutig sein und wird deshalb ggfs. angepasst."
-    >
-      <b-input-group
-        prepend="https://www.nak-sued.stream/"
-      >
-        <b-form-input
-          v-model="requestData.slug"
-          placeholder="gemeinde"
-          :formatter="slugFormatter"
-          type="url"
-          required
-        />
-      </b-input-group>
-    </b-form-group>
-    <b-form-group
-      label="Zugangs-Pin"
-      description="5-stellige Pin die den Zugang schützt. Könnte z.B. identisch mit Perfon-Pin sein."
-    >
-      <b-form-input
-        v-model="requestData.pin"
-        placeholder="12345"
-        type="number"
-        max="99999"
-        min="10000"
-        step="1"
-        :formatter="pinFormatter"
-        required
-      />
-    </b-form-group>
-    <h5>Hinweis</h5>
-    <b-form-group
-      label="Hinweis"
-      description="Allgemeiner Hinweis für die Teilnehmer, der auf der Seite veröffentlicht werden soll."
-    >
-      <b-form-textarea
-        v-model="requestData.Notes"
-        type="text"
-        rows="3"
-        max-rows="6"
-      />
-    </b-form-group>
-    <hr>
-    <h5>Ansprechpartner:in</h5>
-    <p>Die Kontaktinformationen erscheinen für die Zuschauer:innen auf der Pin-geschützten Livestream-Seite.</p>
-    <b-form-group
-      label="Vorname"
-    >
-      <b-form-input
-        v-model="requestData.Firstname"
-        type="text"
-        autocomplete="fname"
-        required
-      />
-    </b-form-group>
-    <b-form-group
-      label="Nachname"
-    >
-      <b-form-input
-        v-model="requestData.Lastname"
-        type="text"
-        autocomplete="lname"
-        required
-      />
-    </b-form-group>
-    <b-form-group
-      label="E-Mail"
-    >
-      <b-form-input
-        v-model="requestData.Mail"
-        type="email"
-        autocomplete="email"
-        required
-      />
+  <section>
+    <h2>Testen und Aufnahme beantragen</h2>
+    <p>
+      Hier kann zum Testen der Link vom Kalender-Export eingegeben werden.
+      Ist er korrekt, so sollte eine Vorschau des Kalenders rechts angezeigt werden.
+    </p>
+    <p>
+      Danach sind noch ein paar Angaben notwendig.
+    </p>
+    <b-form @submit.prevent="testFeed">
       <b-form-group
-        label="Telefon"
-        description="Hier bietet sich eine Mobil-Nummer an, um auch kurzfristig erreichbar zu sein."
+        label="Export Link-Adresse"
       >
-        <b-form-input
-          v-model="requestData.Fon"
-          type="tel"
-          autocomplete="tel"
-          required
-        />
+        <b-input-group>
+          <b-form-input
+            v-model="testingFeed"
+            placeholder="Export Link"
+            description="Der Export-Link aus dem CMS"
+            type="url"
+            required
+          />
+          <b-input-group-append>
+            <b-button
+              variant="primary"
+              type="submit"
+            >
+              Testen
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
       </b-form-group>
-    </b-form-group>
-    <b-button
-      type="submit"
-      variant="primary"
-    >
-      Beantragen
-    </b-button>
-  </b-form>
+    </b-form>
+    <b-row v-if="requestData.Feed">
+      <b-col>
+        <h4>Antrag</h4>
+        <b-form @submit.prevent="submitRequest">
+          <b-form-group
+            label="Export Link-Adresse"
+          >
+            <b-form-input
+              v-model="requestData.Feed"
+              type="url"
+              required
+              disabled
+            />
+          </b-form-group>
+          <b-form-group
+            label="Seiten-Titel"
+            description="z.B. 'Gemeinde Stockach'"
+          >
+            <b-form-input
+              v-model="requestData.Name"
+              placeholder="Gemeinde XYZ"
+              type="text"
+              required
+            />
+          </b-form-group>
+          <b-form-group
+            label="gewünschter Link"
+            description="Gewünschter Link für den Stream. Muss eindeutig sein und wird deshalb ggfs. angepasst."
+          >
+            <b-input-group
+              prepend="https://www.nak-sued.stream/"
+            >
+              <b-form-input
+                v-model="requestData.slug"
+                placeholder="gemeinde"
+                :formatter="slugFormatter"
+                required
+              />
+            </b-input-group>
+          </b-form-group>
+          <b-form-group
+            label="Zugangs-Pin"
+            description="5-stellige Pin die den Zugang schützt. Könnte z.B. identisch mit Perfon-Pin sein."
+          >
+            <b-form-input
+              v-model="requestData.pin"
+              placeholder="12345"
+              type="number"
+              max="99999"
+              min="10000"
+              step="1"
+              :formatter="pinFormatter"
+              required
+            />
+          </b-form-group>
+          <h5>Hinweis</h5>
+          <b-form-group
+            label="Hinweis"
+            description="Allgemeiner Hinweis für die Teilnehmer, der auf der Seite veröffentlicht werden soll."
+          >
+            <b-form-textarea
+              v-model="requestData.Notes"
+              type="text"
+              rows="3"
+              max-rows="6"
+            />
+          </b-form-group>
+          <hr>
+          <h5>Ansprechpartner:in</h5>
+          <p>Die Kontaktinformationen erscheinen für die Zuschauer:innen auf der Pin-geschützten Livestream-Seite.</p>
+          <b-form-group
+            label="Vorname"
+          >
+            <b-form-input
+              v-model="requestData.Firstname"
+              type="text"
+              autocomplete="fname"
+              required
+            />
+          </b-form-group>
+          <b-form-group
+            label="Nachname"
+          >
+            <b-form-input
+              v-model="requestData.Lastname"
+              type="text"
+              autocomplete="lname"
+              required
+            />
+          </b-form-group>
+          <b-form-group
+            label="E-Mail"
+          >
+            <b-form-input
+              v-model="requestData.Mail"
+              type="email"
+              autocomplete="email"
+              required
+            />
+            <b-form-group
+              label="Telefon"
+              description="Hier bietet sich eine Mobil-Nummer an, um auch kurzfristig erreichbar zu sein."
+            >
+              <b-form-input
+                v-model="requestData.Fon"
+                type="tel"
+                autocomplete="tel"
+                required
+              />
+            </b-form-group>
+          </b-form-group>
+          <b-button
+            type="submit"
+            variant="primary"
+          >
+            Beantragen
+          </b-button>
+        </b-form>
+      </b-col>
+      <b-col>
+        <h2>Vorschau</h2>
+        <b-card
+          v-if="requestData"
+        >
+          <hr>
+          <h2>Livestreams</h2>
+          <h3
+            v-text="requestData.Name"
+          />
+          <event-list
+            :feed-url="testingFeed"
+            :url-field="testingUrlField"
+          />
+        </b-card>
+      </b-col>
+    </b-row>
+  </section>
 </template>
 
 <script>
 import congregationService from '@/services/congregation.service'
+import EventList from '@/components/EventList.vue'
 
 export default {
   name: 'RequestForm',
-  props: {
-    request: {
-      type: Object,
-      required: true
-    }
-  },
-
+  components: { EventList },
   data () {
     return {
       requestData: {
-        // Feed: 'https://www.nak-tuttlingen.de/api/tree/38/A86DCC6A-7E88-11EB-9566-BA2F0201BD0E/export'
-      }
+        Feed: null
+      },
+      testingFeed: null,
+      testingUrlField: 'organizer'
     }
   },
   mounted () {
     this.requestData.pin = this.randomIntFromInterval(11111, 99999)
   },
   methods: {
+    testFeed () {
+      this.requestData.Feed = this.testingFeed
+    },
     submitRequest () {
       congregationService.request(this.requestData)
         .then((result) => {
@@ -162,3 +215,10 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/custom-vars.scss';
+.browser-bar {
+  background-color: env(--gray);
+}
+</style>
--- src/views/Request.vue
+++ src/views/Request.vue
@@ -16,89 +16,20 @@
 
     <preparation />
 
-    <section>
-      <h2>Testen und Aufnahme beantragen</h2>
-      <p>
-        Hier kann zum Testen der Link vom Kalender-Export eingegeben werden.
-        Ist er korrekt, so sollte eine Vorschau des Kalenders rechts angezeigt werden.
-      </p>
-      <p>
-        Danach sind noch ein paar Angaben notwendig.
-      </p>
-      <b-form @submit.prevent="testFeed">
-        <b-form-group
-          label="Export Link-Adresse"
-        >
-          <b-input-group>
-            <b-form-input
-              v-model="testingFeed"
-              placeholder="Export Link"
-              description="Der Export-Link aus dem CMS"
-              type="url"
-              required
-            />
-            <b-input-group-append>
-              <b-button
-                variant="primary"
-                type="submit"
-              >
-                Testen
-              </b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-      </b-form>
-      <b-row v-if="requestData.Feed">
-        <b-col>
-          <h4>Antrag</h4>
-          <request-form :request="requestData" />
-        </b-col>
-        <b-col>
-          <b-card>
-            <h2>Vorschau</h2>
-            <hr>
-            <h2>Livestreams</h2>
-            <h3
-              v-text="requestData.Name"
-            />
-            <event-list
-              :feed-url="testingFeed"
-              :url-field="testingUrlField"
-            />
-          </b-card>
-        </b-col>
-      </b-row>
-    </section>
+    <request-form />
   </div>
 </template>
 
 <script>
-import Requirements from '../components/request/Requirements.vue'
-import Preparation from '../components/request/Preparation.vue'
-import RequestForm from '../components/request/RequestForm.vue'
-import EventList from '../components/EventList.vue'
+import Requirements from '@/components/request/Requirements.vue'
+import Preparation from '@/components/request/Preparation.vue'
+import RequestForm from '@/components/request/RequestForm.vue'
 
 export default {
   name: 'Request',
-  components: { Requirements, Preparation, RequestForm, EventList },
+  components: { Requirements, Preparation, RequestForm },
   metaInfo: {
     title: 'Anfrage'
-  },
-  data () {
-    return {
-      requestData: {
-        // Feed: 'https://www.nak-tuttlingen.de/api/tree/38/A86DCC6A-7E88-11EB-9566-BA2F0201BD0E/export'
-      },
-      // TODO testingFeed: null,
-      testingFeed: 'https://www.nak-tuttlingen.de/api/tree/38/A86DCC6A-7E88-11EB-9566-BA2F0201BD0E/export',
-      testingUrlField: 'organizer'
-    }
-  },
-  methods: {
-    testFeed () {
-      console.log('testFeed')
-      this.requestData.Feed = this.testingFeed
-    }
   }
 }
 </script>
