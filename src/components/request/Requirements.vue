c61fcc75e94e54cb50ae49aa2b794b2c14dddff8
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2021-05-10 17:32:15 +0200

    improve requirements

--- src/components/request/Requirements.vue
+++ src/components/request/Requirements.vue
@@ -14,7 +14,15 @@
       <li>
         Es muss ein <strong>Export</strong> für den ensprechenden Termin-Kalender angelegt
         werden können. Hier muss ggfs. der Webmaster des Kirchenbezirks oder des
-        Apostelbereichs unterstützen.
+        Apostelbereichs unterstützen, da nicht alle Anwender die notwendigen Berechtigungen
+        hierfür haben.
+      </li>
+      <li>
+        Um den Teilnehmern eine schnelle Möglichkeit der Kontaktaufnahme zu bieten,
+        werden beim Einrichten <strong>Name</strong>, <strong>Mobil-Nummer</strong> und
+        <strong>E-Mail</strong> abgefragt. Diese Daten erscheinen auch auf der Übersicht
+        der Livestream-Termine. Zusätzlich wird eine <strong>5-stelligen Pin</strong> abgefragt.
+        Hier bietet sich an, die Pin z.B. von Perfon zu übernehmen.
       </li>
     </ol>
   </section>
