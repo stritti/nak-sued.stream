af21b45b251375296b33dc33050c8751073439ff
Stephan Strittmatter <stephan@st-strittmatter.name>
2021-10-02 21:38:16 +0200

    add note to contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="contact">
     <h1>Kontakt</h1>
+    <p>
+      Bitte beachten sie, dass dies ein allgemeines Kontaktformular für die Webseite ist.
+      Wenn sie Fragen zu einem konkreten Stream aus einer Gemeinde haben, nutzen sie bitte die
+      auf der entsprechenden Seite aufgeführten Kontaktinformationen.
+    </p>
     <contact-form />
   </div>
 </template>
