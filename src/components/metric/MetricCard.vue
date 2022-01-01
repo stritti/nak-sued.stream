01a44121a9a8cc2b7a30db4952bf056d64d32cce
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-01-01 22:25:39 +0100

    metrics added

---
+++ src/components/metric/MetricCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-card class="metric text-center">
+    <div class="metric__value">
+      {{ value }}
+    </div>
+    <div class="metric__desc">
+      {{ description }}
+    </div>
+  </b-card>
+</template>
+<script>
+export default {
+  name: 'MetricCard',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "@/assets/scss/custom-vars.scss";
+
+.metric {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #e9ecef;
+  height: 100%;
+  &__value {
+    font-size: 1.75rem;
+    font-weight: bold;
+  }
+  &__desc {
+    font-size: 0.75rem;
+  }
+}
+</style>
